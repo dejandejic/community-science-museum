75102fe561a53d358a016d8742c4ba7dfc7ce3ab
dejandejic <dejood@hotmail.no>
2021-09-28 20:15:41 +0200

    js hamburger

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 //hamburger-menu
-
 function myFunction(){
     var element = document.getElementById("navbar");
-    element.classList.toogle('menu-toogle');
+    element.classList.toggle('menu-toggle');
 }
